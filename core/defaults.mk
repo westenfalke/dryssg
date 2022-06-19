7679c2db2b73f5398060402a0583ace394b4c2a4
westenfalke <7465621+westenfalke@users.noreply.github.com>
2022-06-20 01:07:17 +0200

    fixed auto init om $(MAKE) website_build*

--- core/defaults.mk
+++ core/defaults.mk
@@ -2,7 +2,7 @@ ifndef __gmswe_defaults_included
 __gmswe_defaults_included := $(true)
 
 # set to $(true) to exclude project.mk
-__gmswe_project_included := $(false)
+#__gmswe_project_included := $(false)
 
 #GMSWE_DEBUG  ?= $(true)
 #GMSWE_DOC    ?= $(true)
@@ -18,11 +18,5 @@ CLEAN        ?= clean
 #BASEURL      ?= http://localhost
 DOCUMENTROOT := public_html
 $(eval CLEAN_DOCUMENTROOT = $(CLEAN)_$(DOCUMENTROOT))
-INDEX.HTML   ?= index.html
-$(eval DOCUMENTROOT_INDEX.HTML = $(DOCUMENTROOT)/$(INDEX.HTML))
-INDEX.MD     ?= index.md
-$(eval DOCUMENTS_INDEX.MD ?= $(DOCUMENTS)/$(INDEX.MD))
-SITEMAP.XML  ?= sitemap.xml
-$(eval DOCUMENTROOT_SITEMAP.XML = $(DOCUMENTROOT)/$(SITEMAP.XML))
 
 endif #__gmswe_defaults_included
--- modules/_modules.mk
+++ modules/_modules.mk
@@ -4,8 +4,8 @@ include modules/trace/trace.mk
 include modules/debug/debug.mk
 include modules/logging/logging.mk
 include modules/documents/documents.mk
-include modules/robots_txt/robots_txt.mk
 include modules/sitemap_xml/sitemap_xml.mk
+include modules/robots_txt/robots_txt.mk
 include modules/markdown2html/markdown2html.mk
 include modules/menu_card/menu_card.mk
 include modules/test/test.mk
